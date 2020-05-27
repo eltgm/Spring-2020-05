485668e803a0816694cd2b99a2363e77527fcdc8
eltgm <freestyle1448@gmail.com>
2020-05-27 23:31:00 +0300

    ленивая инициализация вопросов

--- hw01-intro/src/main/java/ru/otus/dao/ExercisesDAOImpl.java
+++ hw01-intro/src/main/java/ru/otus/dao/ExercisesDAOImpl.java
@@ -10,13 +10,28 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ExercisesDAOImpl implements ExercisesDAO {
+    private final String fileName;
+    private boolean isInit = false;
     private static final String COMMA_DELIMITER = ",";
     private final List<Exercise> exercises = new ArrayList<>();
 
     public ExercisesDAOImpl(String exercisesFileName) {
+        this.fileName = exercisesFileName;
+    }
+
+    @Override
+    public List<Exercise> getAllExercises() {
+        if (!isInit) {
+            init();
+        }
+
+        return exercises;
+    }
+
+    public void init() {
         List<List<String>> records = new ArrayList<>();
 
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream(exercisesFileName)))) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream(fileName)))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] values = line.split(COMMA_DELIMITER);
@@ -24,13 +39,10 @@ public class ExercisesDAOImpl implements ExercisesDAO {
             }
         } catch (IOException e) {
             e.printStackTrace();
+            return;
         }
 
+        isInit = true;
         records.forEach(strings -> exercises.add(new Exercise(strings.get(0), strings.subList(1, strings.size()))));
-    }
-
-    @Override
-    public List<Exercise> getAllExercises() {
-        return exercises;
     }
 }
--- hw01-intro/src/main/java/ru/otus/service/StudentTestingServiceImpl.java
+++ hw01-intro/src/main/java/ru/otus/service/StudentTestingServiceImpl.java
@@ -16,25 +16,21 @@ public class StudentTestingServiceImpl implements StudentTestingService {
         final var stringBuilder = new StringBuilder();
 
         stringBuilder.append("\n");
-        if (!allExercises.isEmpty()) {
-            for (Exercise exercise : allExercises) {
-                final var answers = exercise.getAnswers();
-                if (answers.size() == 1) {
-                    stringBuilder.append(String.format("Write: %s%n ", exercise.getQuestion()));
-                } else {
-                    stringBuilder.append(String.format("Choose: %s%n", exercise.getQuestion()));
+        for (Exercise exercise : allExercises) {
+            final var answers = exercise.getAnswers();
+            if (answers.size() == 1) {
+                stringBuilder.append(String.format("Write: %s%n ", exercise.getQuestion()));
+            } else {
+                stringBuilder.append(String.format("Choose: %s%n", exercise.getQuestion()));
 
-                    var answerNumber = 1;
-                    for (String answer : answers) {
-                        stringBuilder.append(String.format("  %d.%s;%n", answerNumber, answer));
-                        answerNumber++;
-                    }
+                var answerNumber = 1;
+                for (String answer : answers) {
+                    stringBuilder.append(String.format("  %d.%s;%n", answerNumber, answer));
+                    answerNumber++;
                 }
-
-                stringBuilder.append("\n");
             }
-        } else {
-            stringBuilder.append("Failed to load questions!");
+
+            stringBuilder.append("\n");
         }
 
         return stringBuilder.toString();
